82320b57ec590ee02607c3113d250a77388e0257
artjom <artemdobrom@gmail.com>
2023-08-07 19:48:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,586 @@
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//        double dog = 8;
+//        var cat = 3.6;
+//        var paper = 763_789;
+//        System.out.println("dog = " + dog);
+//        System.out.println("cat = " + cat);
+//        System.out.println("paper = " + paper);
+//
+//        dog = dog + 4;
+//        cat = cat + 4;
+//        paper = paper + 4;
+//
+//        System.out.println();
+//        System.out.println("dog = " + dog);
+//        System.out.println("cat = " + cat);
+//        System.out.println("paper = " + paper);
+//
+//        dog = dog - 3.5;
+//        cat = cat - 1.6;
+//        paper = paper - 7_639;
+//        System.out.println();
+//        System.out.println("dog = " + dog);
+//        System.out.println("cat = " + cat);
+//        System.out.println("paper = " + paper);
+//        System.out.println();
+//
+//        var friend = 19;
+//        System.out.println("friend = " + friend);
+//        friend = friend + 2;
+//        System.out.println("friend +2 = " + friend);
+//        friend = friend / 7;
+//        System.out.println("friend /7= " + friend);
+//        System.out.println();
+//
+//        var frog = 3.5;
+//        System.out.println("frog = " + frog);
+//        frog = frog * 10;
+//        System.out.println("frog * 10 = " + frog);
+//        frog = frog / 3.5;
+//        System.out.println("frog / 3.5 = " + frog);
+//        frog = frog + 4;
+//        System.out.println("frog +4 = " + frog);
+//        System.out.println();
+//
+//        var weightBoxerOne = 78.2;
+//        var weightBoxerTwo = 82.7;
+//
+//        var totalWeightBoxers = weightBoxerOne + weightBoxerTwo;
+//        System.out.println("totalWeightBoxers = " + totalWeightBoxers);
+//
+//        var weightDifferenceBoxers = weightBoxerTwo - weightBoxerOne;
+//        System.out.println("weightDifferenceBoxers = " + weightDifferenceBoxers);
+//
+//        var weightDifferenceBoxersTwo = weightBoxerTwo % weightBoxerOne;
+//        System.out.println("weightDifferenceBoxersTwo = " + weightDifferenceBoxersTwo);
+//        System.out.println();
+//
+//        var totalWorkHours = 640;
+//        var normalWorkHoursPerDay = 8;
+//
+//        var totalWorkers = totalWorkHours / normalWorkHoursPerDay;
+//        System.out.println("totalWorkers = " + totalWorkers);
+//
+//        var totalWorkersNew = totalWorkers + 94;
+//        var totalWorkHoursNew = totalWorkersNew * 8;
+//        System.out.println("Если в компании работает " + totalWorkersNew + " человек, то всего " + totalWorkHoursNew + " часов работы может быть поделено между сотрудниками.");
+//        System.out.println();
+//
+//        int a = 987_678_965;
+//        byte b = 67;
+//        short c = 569;
+//        short c1 = 27_897;
+//        short c2 = -159;
+//        long d = 987_678_965_549l;
+//        float e = 27.12f;
+//        float e1 = 2.786f;
+//        double f = 12.121212d;
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+//        System.out.println("c = " + c);
+//        System.out.println("d = " + d);
+//        System.out.println("e = " + e);
+//        System.out.println("f = " + f);
+//
+//        int Ljudmila = 23;
+//        int Anna = 27;
+//        int Ekaterina = 30;
+//        int totalPaper = 480;
+//
+//        int paperForOneStudent = totalPaper / (Ljudmila + Anna + Ekaterina);
+//        System.out.println("paperForOneStudent = " + paperForOneStudent);
+//        System.out.println();
+//
+//        int bottleProductionInOneMinute = 16 / 2;
+//        int bottleProductionIn20Minutes = bottleProductionInOneMinute * 20;
+//        int bottleProductionInDay = bottleProductionInOneMinute * 60 * 24;
+//        int bottleProductionIn3Days = bottleProductionInDay * 3;
+//        int bottleProductionInOneMount = bottleProductionIn3Days * 10;
+//        System.out.println("bottleProductionInOneMinute = " + bottleProductionInOneMinute);
+//        System.out.println("bottleProductionIn20Minutes = " + bottleProductionIn20Minutes);
+//        System.out.println("bottleProductionInDay = " + bottleProductionInDay);
+//        System.out.println("bottleProductionIn3Days = " + bottleProductionIn3Days);
+//        System.out.println("bottleProductionInOneMount = " + bottleProductionInOneMount);
+//        System.out.println();
+//
+//
+//        int totalPaintCans = 120;
+//        int oneClassPaintWhiteCans = 2;
+//        int oneClassPaintBrownCans = 4;
+//
+//        int totalClasses = totalPaintCans / (oneClassPaintBrownCans + oneClassPaintWhiteCans);
+//        System.out.println("totalClasses = " + totalClasses);
+//
+//        int totalBrownPaintCans = totalClasses * oneClassPaintBrownCans;
+//        int totalWhitePaintCans = totalClasses * oneClassPaintWhiteCans;
+//        System.out.println("totalBrownPaintCans = " + totalBrownPaintCans);
+//        System.out.println("totalWhitePaintCans = " + totalWhitePaintCans);
+//        System.out.println();
+//
+//        ////Sport cocktail
+//        float bananas = 5 * 80;
+//        float milk = 100 / 105f * 2f;
+//        float iceCream = 2 * 100;
+//        float egs = 4 * 70;
+//
+//        float cocktail = bananas = milk + iceCream + egs;
+//        System.out.println("cocktail = " + cocktail);
+//        float cocktailKg = cocktail / 1000;
+//        System.out.println("cocktailKg = " + cocktailKg);
+//        System.out.println();
+//
+//
+//        //Tournament Weight
+//        int needToLose = 7;
+//        float loseDayOne = 250 / 1000f;
+//        float loseDayTwo = 500 / 1000f;
+//
+//        float daysA = needToLose / loseDayOne;
+//        float daysB = needToLose / loseDayTwo;
+//
+//        System.out.println("daysA = " + daysA);
+//        System.out.println("daysB = " + daysB);
+//        System.out.println();
+//
+//        //company
+//        float salaryUp = 1.1f;
+//        float mashaSalary = 67_760f;
+//        float denisSalary = 83_690f;
+//        float kristinaSalary = 76_230f;
+//
+//        float newSalaryMasha = mashaSalary * salaryUp;
+//        float newSalaryDenis = denisSalary * salaryUp;
+//        float newSalaryKristina = kristinaSalary * salaryUp;
+//        System.out.println("newSalaryMasha = " + newSalaryMasha);
+//        System.out.println("newSalaryDenis = " + newSalaryDenis);
+//        System.out.println("newSalaryKristina = " + newSalaryKristina);
+//
+//        float differenceSalaryMasha = newSalaryMasha - mashaSalary;
+//        float differenceSalaryDenis = newSalaryDenis - denisSalary;
+//        float differenceSalaryKristina = newSalaryKristina - kristinaSalary;
+//        System.out.println("differenceSalaryMasha = " + differenceSalaryMasha);
+//        System.out.println("differenceSalaryDenis = " + differenceSalaryDenis);
+//        System.out.println("differenceSalaryKristina = " + differenceSalaryKristina);
+//        System.out.println();
+//
+//        //Conditionals
+//        int age = 19;
+//        System.out.print("Если возраст человека равен ");
+//        if (age >= 18) {
+//            System.out.print(age + " ,то он совершеннолетний ");
+//        }
+//        if (age < 18) {
+//            System.out.print(age + " ,то он не совершеннолетний");
+//        }
+//
+//
+//        //Lessen 2
+//        int temperature = 10;
+//        System.out.println("");
+//        System.out.println("");
+//        if (temperature <= 5) {
+//            System.out.println("На улице холодно, нужно надеть шапку");
+//        }
+//        if (temperature > 5) {
+//            System.out.println("Сегодня тепло, можно идти без шапки");
+//        }
+//
+//        System.out.println();
+//
+//        //Lessen3
+//        int speed = 20;
+//        System.out.print("Если скороксть " + speed);
+//        if (speed > 60) {
+//            System.out.println(" Придётся заплатить штраф");
+//        }
+//        if (speed <= 60) {
+//            System.out.println(" Можно ездить спокойно");
+//        } else {
+//            System.out.println(" Не корректный возраст");
+//        }
+//        System.out.println();
+//
+//        //Lessen 4
+//        int age1 = 120;
+//        System.out.print("Если возраст человека равен " + age1);
+//        if (age1 <= 2) {
+//            System.out.println(" пора спать");
+//        }
+//        if (age1 > 2 && age1 <= 6) {
+//            System.out.println(" Нужно ходить в сад");
+//        }
+//        if (age1 >= 7 && age1 < 18) {
+//            System.out.println(" нужно ходить в школу");
+//        }
+//        if (age1 >= 18 && age1 < 24) {
+//            System.out.println(" место в универе");
+//        }
+//        if (age1 >= 24 && age1 < 60) {
+//            System.out.println(" то ему пора работать");
+//        }
+//        if (age1 >= 60) {
+//            System.out.println(" то можно отдахнуть");
+//        } else {
+//            System.out.println("не корректный возраст");
+//        }
+//        System.out.println();
+//
+//
+//        //Lessen5
+//        int age2 = 105;
+//        System.out.print("Если возраст человека равен " + age2);
+//        if (age2 < 5) {
+//            System.out.println(" то ему нельзя кататься на атракционе");
+//        }
+//        if (age2 >= 5 && age2 < 14) {
+//            System.out.println(" то ему можно кататься в сопровождении");
+//        }
+//        if (age2 >= 14) {
+//            System.out.println(" то ему можно кататься без сопровождения");
+//        } else {
+//            System.out.println("не корректный возраст");
+//        }
+//        System.out.println();
+//
+//        // Lessen 5.5
+//
+//        boolean skyIsClean = false;
+//        if (skyIsClean) {
+//            System.out.println("Небо чистое");
+//        } else {
+//            System.out.println("Небо туманное");
+//        }
+//        System.out.println();
+//
+//        //Lessen 6
+//        int place = 111;
+//        boolean seatPlace = place >= 0 && place <= 60;
+//        boolean standingPlace = place > 60 && place <= 102;
+//        if (seatPlace) {
+//            System.out.println("В вагоне есть сидячие места");
+//        }
+//        if (standingPlace) {
+//
+//            System.out.println("В вагоне есть стоячие места");
+//        }
+//        if (place < 0 || place > 102) {
+//            System.out.println("в вагоне нет мест");
+//        }
+//        System.out.println();
+//
+//        //Lesson7
+//
+//        {int one = 1;
+//        int two = 2;
+//        int three = 3;
+//        if (one > two && one > three) {
+//            System.out.println("самое большое число" + one);
+//        } else if (two > one && two > three) {
+//            System.out.println("самое большое число " + two);
+//        } else {
+//            System.out.println("самое большое число " + three);
+//        }
+//        System.out.println();}
+//
+//
+//        //iOs & Android
+//        boolean clientIOs = true;
+//        int clientDeviceYear = 2016;
+//        if (clientIOs) {
+//            System.out.println("Установите версию приложения для iOS по ссылке");
+//        } else {
+//            System.out.println("Установите версию приложения для Android по ссылке");
+//        }
+//        if (clientDeviceYear <= 2015) {
+//            System.out.println("Установите облегчённую версию");
+//        }
+//        System.out.println();
+//
+//
+//        //Years
+//
+//        int year = 2021;
+//        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+//            System.out.println(year + " год является высокосным");
+//        } else {
+//            System.out.println(year + " год не является высокосным");
+//        }
+//        System.out.println();
+//
+//        //bank
+//
+//        int deliveryDistance = 95;
+//        boolean deliveryDistance20to60 = deliveryDistance > 20 && deliveryDistance <= 60;
+//        boolean deliveryDistance60to100 = deliveryDistance > 60 && deliveryDistance <= 100;
+//
+//        if (deliveryDistance <= 20) {
+//            System.out.println("Потребуется 1 день");
+//        } else if (deliveryDistance20to60) {
+//            System.out.println("Потребуется 2 дня");
+//        } else if (deliveryDistance60to100) {
+//            System.out.println("Потребуется 3 дня");
+//        } else {
+//            System.out.println("Доставки нет");
+//        }
+//        System.out.println();
+//
+//
+//        //Number of the month in the year
+//
+//        int monthNumber = 5;
+//        switch (monthNumber) {
+//            case 12:
+//            case 1:
+//            case 2:
+//                System.out.println("Зима");
+//                break;
+//            case 3:
+//            case 4:
+//            case 5:
+//                System.out.println("Весна");
+//                break;
+//            case 6:
+//            case 7:
+//            case 8:
+//                System.out.println("Лето");
+//                break;
+//            case 9:
+//            case 10:
+//            case 11:
+//                System.out.println("Осень");
+//                break;
+//            default:
+//                System.out.println("Нет такого месяца");
+//        }
+//        System.out.println();
+        //Cycles
+
+//
+//        for (int i = 1; i <= 10; i = i + 1) {
+//            System.out.printf(" " + i);
+//        }
+//        System.out.println();
+//
+//        //Lesson2
+//        for (int i = 10; i >= 1; i = i - 1) {
+//            System.out.printf(" " + i);
+//        }
+//        System.out.println();
+//        System.out.println();
+//
+//        //Lesson3
+//        {int i = 0;
+//        for (; i <= 17; i = i + 1) {
+//            if (i % 2 == 0) {
+//                System.out.printf(" "+i);}
+//            }
+//        }
+//        System.out.println();
+//        for (int j = 0; j <= 17; j = j + 2) {
+//            System.out.printf(" "+j);
+//        }
+//        System.out.println();
+//        System.out.println();
+//
+//        //Lesson 4
+//
+//        for (int i = 10; i >= -10; i = i - 1) {
+//            System.out.printf(" "+i);
+//        }
+//        System.out.println();
+//        System.out.println();
+//
+//        //lesson5
+//
+//        int i = 1904;
+//        for (; i <= 2096; i++) {
+//            if (i % 4 == 0 && i % 100 !=0 || i % 400 == 0) {
+//                System.out.println(i+ " год является высокосным.");
+//            }
+//        }
+//        System.out.println();
+//
+//
+//        //lesson6
+//        for (i = 7; i <= 98; i = i + 7) {
+//            System.out.printf(" "+i);
+//        }
+//        System.out.println();
+//        System.out.println();
+//
+//        //lesson7
+//
+//        for (i = 1; i <= 512; i = i + i) {
+//            System.out.printf(" " + i);
+//        }
+//        System.out.println();
+//        System.out.println();
+
+
+//        //lesson 8
+//
+//        int salary = 29_000;
+//        float total = 0;
+//        float percent = 0.01f;
+//        float stop = 2_459_000;
+//        int i = 1;
+//
+//        while ( total<=stop) {
+//            total = total + percent*total;
+//            total = total + salary;
+//            System.out.println("Месяц "+i+ " ,сумма накоплений равна "+ total+ " рублей.");
+//            i = i + 1;
+//        }
+//        System.out.println();
+//        System.out.println();
+
+//        //lesson 10
+//        int number = 1;
+//        for (i = 1; i <= 10; i++) {
+//
+//            System.out.println("2*"+i+"="+i*2);
+//        }
+//        System.out.println();
+//        System.out.println();
+//
+//        //lesson 2.1
+/*
+        int i = 0;
+        while ( i < 10) {
+            i = i + 1;
+            System.out.printf(" "+i);
+        }
+        System.out.println();
+        for (i = 10; i >= 1; i = i - 1) {
+            System.out.printf(" "+i);
+        }*/
+
+
+      //LESSON 2.3
+//        int y = 12_000_000;
+//        int birthRate =17;
+//        int deadRate = - 8;
+//
+//
+//        for (int i = 1; i <= 10; i++) {
+//                y = y + y / 1000 * birthRate + y / 1000 * deadRate;
+//            System.out.println("год "+i+" численность населения составляет "+ y);
+//        }
+
+
+        //lesson 2.4
+//        float percent = 0.07f;
+//        float start = 15_000;
+//        int finish = 12_000_000;
+//        int totalMonth= 9*12;
+//
+//        for (int i=1; i<=totalMonth; i++) {
+//            start = start + start * percent;
+//            if (i % 6 == 0 ) {
+//                System.out.println(i + " месяц " + start + " рублей");
+//            }
+//        }
+//        System.out.println();
+//
+//
+//        //lesson 7
+//
+//        int firstFridayOfTheMonth = 3;
+//
+//        for (int i = firstFridayOfTheMonth;i<=31 ; i=i+7) {
+//
+//                System.out.println("Сегодня пятница "+ i+ "-е число. Необходимо подготовить отчёт.");
+//        }
+//        System.out.println();
+//
+//        //lesson 8
+//
+//        int toDay = 2023;
+//        int first = 79;
+//        int last200years = 200;
+//        int future100years = 100;
+//
+//        for (int i = 0; i <= toDay+future100years; i=i+first) {
+//            if (i > (toDay - last200years) && i < (toDay + future100years)) {
+//
+//                System.out.println(i);
+//            }
+//        }
+
+
+//        //Lesson Arrays
+//
+//        int[] a = new int[3];
+//        a [0] = 1;
+//        a [1] = 2;
+//        a[2] = 3;
+//
+//        for (int i =0; i < a.length; i++) {
+//            if (a[i] % 2 != 0) {
+//                a[i]++;
+//            }
+//        }
+//        System.out.println(Arrays.toString(a));
+//
+
+
+
+//
+//        float[] b = {1.57f, 7.654f, 9.986f};
+//        System.out.println(Arrays.toString(b));
+//        for (int i = b.length - 1; i >= 0; i--) {
+//            System.out.print(b[i]);
+//            if (i != 0) {
+//                System.out.print(",");
+//            }
+//        }
+//        System.out.println();
+//
+//
+//        int[] c = {2, 4, 6};
+//            System.out.println(Arrays.toString(c));
+//        for (int i = c.length-1; i >=0; i--) {
+//            System.out.print(c[i]);
+//            if (i != 0) {
+//                System.out.print(",");
+//            }
+//        }
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        //Lesson1
+        int[] salaryTotal = generateRandomArray();
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
